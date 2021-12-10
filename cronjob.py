f83ce7d302d3692a7eff3489c095bfff8b7d44ea
pbyrne96 <p.byrne@weareeight.com>
2021-12-10 17:57:05 +0000

    fixing cronjob

--- cronjob.py
+++ cronjob.py
@@ -2,4 +2,6 @@ from apscheduler.schedulers.background import BlockingScheduler
 from send_alerts import send_alert
 
 scheduler = BlockingScheduler()
-scheduler.add_job(send_alert('home_fixtures.json').__main__(), "interval", seconds=(86400//2))
+cls = send_alert('home_fixtures.json')
+scheduler.add_job(cls.__main__, "cron", day_of_week="mon-sun",hour="09")
+scheduler.start()
